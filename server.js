f1058a8433163f9b7b743502152ce6ca4834e1ec
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-04-13 14:44:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const staticFolder = 'public';
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(staticFolder));
 
 app.use('/api', apiRoutes);
 
@@ -25,5 +27,3 @@ async function bootServer() {
 }
 
 bootServer();
-const staticFolder = 'public';
-app.use(express.static(staticFolder));
