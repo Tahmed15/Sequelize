0fc9ea625889f17d6be260dbe67ba43a98f7c903
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-04-13 15:22:33 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
+const { application } = require("express");
+
 async function dininghalls(){
     const request = await fetch("/api/dining");
     const data = await request.json();
 }
+
+const staticFolder = 'public';
+app.use(express.static(staticFolder));
+app.use('/api', apiRoutes);
